22fb31191565978ee39475a0f09317c4c4a6ad61
maragao <mariazevedoaragao@gmail.com>
2022-05-17 01:34:59 -0300

    add

---
+++ ft_calloc.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_calloc.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: maragao <maragao@student.42.rio>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/17 01:12:01 by maragao           #+#    #+#             */
+/*   Updated: 2022/05/17 01:33:16 by maragao          ###   ########.rio      */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_calloc(size_t nmemb, size_t size)
+{
+	void	*ptr;
+	size_t	tot_size;
+
+	tot_size = nmemb * size;
+	ptr = (void *) malloc (tot_size);
+	if (!ptr)
+		return (NULL);
+	ft_bzero (ptr, tot_size);
+	return (ptr);
+}
